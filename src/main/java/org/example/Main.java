7ecdd15c8d2edabe26a6457a6c7de497966d0c9d
Sanjida <sanji_b@outlook.com>
2024-07-23 17:53:33 +0100

    added try-catch block for cities

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,34 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        System.out.println("Hello");
-        System.out.println("distance");
+        String file = "src\\worldCities.csv";
+        BufferedReader reader = null;
+        String line = "";
+
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            while ((line = reader.readLine()) != null) {
+
+                String[] row = line.split((","));
+                for(String index: row) {
+                    System.out.printf("%10s", index);
+                }
+                System.out.println();
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        finally{
+            reader.close();
+        }
     }
 }
