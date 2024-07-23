8a215a690a93874b392d14818d1ba1fff2436629
Sanjida <sanji_b@outlook.com>
2024-07-23 14:12:32 +0100

    stored variables in City class

--- src/main/java/City.java
+++ src/main/java/City.java
@@ -1,3 +1,30 @@
-public class City {
 
+public class City {
+    private String cityName;
+    private double x;
+    private double y;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
 }
