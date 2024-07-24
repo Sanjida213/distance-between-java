0ca0afad4d1f4d58b55779ae668ccdac80c54ea3
Sanjida <sanji_b@outlook.com>
2024-07-24 15:56:30 +0100

    userInput created

---
+++ src/main/java/UserInput.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class UserInput {
+    private Scanner scanner;
+
+    public UserInput() {
+        scanner = new Scanner(System.in);
+    }
+
+    public String getCityName(String prompt) {
+        System.out.println(prompt);
+        return scanner.nextLine().trim();
+    }
+
+    public void closeScanner() {
+        scanner.close();
+    }
+}
